f3d7eb660abe8b2dd31151a62f1b93769bfb6a81
Jaroslav Kasparek <Jaroslav.Kasparek@finfactory.com>
2019-02-18 23:10:34 +0100

    Fix a typo

--- lib/zabbix.rb
+++ lib/zabbix.rb
@@ -1,27 +1,27 @@
 module Zabbix
 end
 
-require "socket" 
+require "socket"
 require "yajl"
 require "zabbix/agent/configuration"
 require "zabbix/sender"
 require "zabbix/sender/easy"
 require "zabbix/sender/buffer"
 
-## we could use zabbix-sender, but that appears to be unecessary; we can instead 
-## simply talk to the zabbit-server. we get there via the parent. 
-## 
+## we could use zabbix-sender, but that appears to be unecessary; we can instead
+## simply talk to the zabbix-server. we get there via the parent.
+##
 ## thinking about what we want to be able to do there are a few types of messages
 ## we want to send to zabbix:
-##   1. that a process has starting 
-##   2. that a process is running 
+##   1. that a process has starting
+##   2. that a process is running
 ##   3. that a process has finished 
-## 
-## there will be more in the future, but this is the minimum; it would be nice 
-## to expose two interfaces: 
+##
+## there will be more in the future, but this is the minimum; it would be nice
+## to expose two interfaces:
 
 ## Zabbix::Sender.run(:task_name, opts={}) do |zbx|
-##   ... 
+##   ...
 ##   zbx.heartbeat
 ## end
 
@@ -30,5 +30,4 @@ require "zabbix/sender/buffer"
 ## zbx.send_heartbeat
 ## zbx.send_end
 
-## That's pretty much it. 
-
+## That's pretty much it.
